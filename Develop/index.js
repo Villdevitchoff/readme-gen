7de0291bf47a2d18d7148cdcffb40057d1b55697
unknown <bkinrewin@gmail.com>
2022-02-06 12:58:50 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+const Choices = require('inquirer/lib/objects/choices');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a brief description of your project' 
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How will the user access this application?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the best use for this application?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the best license associated with your project',
+        choices: ['MIT', 'GPL 3.0', 'Apache', 'BSD', 'N/A']
+    },
+    {
+        type: 'inupt',
+        name: 'test',
+        message: 'Please enter tests to use for your project'
+    },
+    {
+        type: 'input',
+        name: 'question',
+        message: 'Please provide your email for questions',
+        validate:nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please provide your email!')
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide your github account'
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
